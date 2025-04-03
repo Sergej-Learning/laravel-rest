ef10010a296a0478578d22b4d10b28840c62c98e
Sergej Minor <sminor-gera@web.de>
2025-04-03 11:11:40 +0200

    Stand Mitte : 03.04.2025

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\LabelController;
+use App\Http\Controllers\SongController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('songs', SongController::class);
+Route::resource('labes', LabelController::class);
